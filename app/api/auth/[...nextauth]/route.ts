24c394c10b2d68a55ed5c499644c0f79735c3991
Ahd-Kabeer-Hadi <ahd.kabeerpi@gmail.com>
2024-08-07 18:48:55 +0530

    Fix: build error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,8 +3,7 @@ import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { NextApiHandler } from "next";
 
-// Create a handler for NextAuth
-const handler: NextApiHandler = (req, res) => NextAuth(req, res, authOptions);
-
+//@ts-ignore
+const handler = NextAuth(authOptions)
 // Export HTTP method handlers
 export { handler as GET, handler as POST };
